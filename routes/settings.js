beb450cf294dc69a1cfefd687b43b89c6ed25ed8
Idento <max.78.ch@gmail.com>
2025-10-15 10:00:41 +0200

    Better storage of function and road

--- routes/settings.js
+++ routes/settings.js
@@ -1,9 +1,19 @@
 import express, { Router } from 'express';
-import { GetDrivers, ModifyDrivers, DeleteDrivers, AddDrivers, DeleteAccount, GetHistoryData, ModifyAccount } from '../controllers/settingsController.js';
+import {
+    GetDrivers,
+    ModifyDrivers,
+    DeleteDrivers,
+    AddDrivers,
+    DeleteAccount,
+    GetHistoryData,
+    ModifyAccount,
+    createUser,
+    changePassword,
+    regenPassword,
+    CheckUserName
+} from '../controllers/settingsController.js';
 import { isAuthAndAdmin, isAuthentified } from '../middlewares/sessionCheck.js';
-import { createUser, changePassword, regenPassword, CheckUserName } from '../controllers/userController.js';
 import { getUsers } from '../controllers/userController.js';
-
 
 
 const router = Router();
@@ -13,11 +23,11 @@ router.get('/getUser', isAuthentified, getUsers);
 router.post('/getHistory', isAuthentified, GetHistoryData);
 router.post('/CheckName', isAuthentified, CheckUserName)
 router.post('/add', isAuthentified, AddDrivers);
+router.post('/addAccount', isAuthAndAdmin, createUser);
 router.post('/modify', isAuthentified, ModifyDrivers);
 router.post('/modifyAccount', isAuthentified, ModifyAccount);
 router.post('/changePassword', isAuthentified, changePassword);
 router.post('/regeneratePassword', isAuthAndAdmin, regenPassword);
-router.post('/addAccount', isAuthAndAdmin, createUser);
 router.delete('/delete', isAuthentified, DeleteDrivers);
 router.delete('/deleteAccount', isAuthAndAdmin, DeleteAccount);
 
