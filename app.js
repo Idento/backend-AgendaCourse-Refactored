6ddd33555427116fae406016260d7ccb37971546
Idento <max.78.ch@gmail.com>
2025-11-14 21:31:02 +0100

    Back to http

--- app.js
+++ app.js
@@ -17,33 +17,27 @@ import { checkOldPlanning, checkPlanningRecurrence } from './services/PlanningSe
 import { checkRecurrenceStartDates } from './services/ReccurenceService.js';
 import { savePlanning } from './services/ArchiveService.js';
 import dotenv from 'dotenv';
-import helmet from 'helmet';
 dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
-app.use(helmet({
-    contentSecurityPolicy: false,
-    crossOriginEmbedderPolicy: false,
-    crossOriginResourcePolicy: { policy: "cross-origin" },
-    referrerPolicy: { policy: "no-referrer" },
-}))
+
 export const sessionMiddleware = session({
     secret: process.env.SECRET,
     resave: true,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        secure: true,
-        maxAge: 14400000
+        secure: false,
+        maxAge: 14400000,
     }
 })
-app.set('trust proxy', 1)
+
 export const corsOptions = cors({
     origin: ['http://localhost:5173', 'http://localhost:5174'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
+    allowedHeaders: ['Content-Type', "application/javascript"],
     credentials: true
 })
 app.use(corsOptions)
